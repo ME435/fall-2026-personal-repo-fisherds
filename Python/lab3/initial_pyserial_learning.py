3c4728c83c8082ae5abf9c0b57665be6f0d5e719
David Fisher <fisherds@rose-hulman.edu>
2026-09-21 14:18:59 -0400

    code from my computer

--- Python/lab3/initial_pyserial_learning.py
+++ Python/lab3/initial_pyserial_learning.py
@@ -1,13 +1,23 @@
 import serial
+import time
 
 print("Learning Pyserial")
 
 ser = serial.Serial(port="/dev/ttyACM0", baudrate=19200, timeout=10)
 
-while not ser.is_open:
-    print("Opening...")
+time.sleep(2.0) # Necessary sometimes. :)
 
-# TODO: User the ser object
+ser.reset_input_buffer()
+message = "RESET"
+print(message)
+message_bytes = (message + "\n").encode()
+print(message_bytes)
 
+ser.write(message_bytes)
+
+response_bytes = ser.readline()
+print(response_bytes)
+response = response_bytes.decode().strip()
+print(response)
 
 ser.close()
